7951af600c9ff807790878459bf79ee25e10499f
Alexandre Kremlianski <kremlianski@gmail.com>
2017-04-11 20:22:40 +0300

    some changes

--- src/main/scala-2.12/net/scalapro/liftportal/snippet/SpaceSnippet.scala
+++ src/main/scala-2.12/net/scalapro/liftportal/snippet/SpaceSnippet.scala
@@ -2,9 +2,11 @@ package net.scalapro.liftportal.snippet
 
 import net.liftweb.http.S
 import net.scalapro.liftportal.view.TemplateView._
+import net.scalapro.liftportal.cms.views.TWidgetV
 
 import scala.xml.{NodeSeq, Unparsed}
 import net.liftweb.util.Helpers._
+import net.scalapro.liftportal.cms.tables.Widgets
 import net.scalapro.liftportal.cms.views.TemplateV
 import net.scalapro.liftportal.util.DB
 
@@ -16,7 +18,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 //import net.scalapro.liftportal.util.FutureBinds._
 import scala.util.{Failure, Success}
 
-import scala.xml.{Elem, Node, XML}
+import scala.xml.{Elem, Node, XML, Text}
 /**
   * Created by kreml on 21.03.2017.
   */
@@ -28,7 +30,9 @@ class SpaceSnippet {
     val spaces = spacesStorage.is
     val id = S.attr("id").openOr("0")
 
-    val space = spaces.get(id.toInt).getOrElse(Map.empty)
+    val space = spaces.get(id.toInt).getOrElse(Map.empty).asInstanceOf[Seq[TWidgetV]]
+
+
 
 
     "*" #> <div class="space" data-xx-sid={id}>
