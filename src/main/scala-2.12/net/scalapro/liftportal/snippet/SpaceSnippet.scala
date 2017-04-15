88b32bab41760a1f34170782060e9c932bf2dccd
Alexandre Kremlianski <kremlianski@gmail.com>
2017-04-15 11:07:19 +0300

    some changes

--- src/main/scala-2.12/net/scalapro/liftportal/snippet/SpaceSnippet.scala
+++ src/main/scala-2.12/net/scalapro/liftportal/snippet/SpaceSnippet.scala
@@ -1,24 +1,8 @@
 package net.scalapro.liftportal.snippet
 
 import net.liftweb.http.S
-import net.scalapro.liftportal.view.TemplateView._
-import net.scalapro.liftportal.cms.views.TWidgetV
-
-import scala.xml.{NodeSeq, Unparsed}
 import net.liftweb.util.Helpers._
 import net.scalapro.liftportal.cms.tables.Widgets
-import net.scalapro.liftportal.cms.views.TemplateV
-import net.scalapro.liftportal.util.DB
-
-import scala.concurrent.Await
-import slick.jdbc.PostgresProfile.api._
-
-import scala.concurrent.duration.Duration
-import scala.concurrent.ExecutionContext.Implicits.global
-//import net.scalapro.liftportal.util.FutureBinds._
-import scala.util.{Failure, Success}
-
-import scala.xml.{Elem, Node, XML, Text}
 import net.scalapro.liftportal.util.Vars.spacesStorage
 
 /**
